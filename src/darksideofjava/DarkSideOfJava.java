678ac1c95221b1a7fa718579e7efa57cc075cb76
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-03-01 10:47:58 +0400

    first commit

---
+++ src/darksideofjava/DarkSideOfJava.java
@@ -0,0 +1,17 @@
+ 
+package darksideofjava;
+
+/**
+ *
+ * @author teyyu
+ */
+public class DarkSideOfJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
